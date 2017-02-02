f852e3eca788c3f2828d8540aafa4ddee499255c
chadapon <chadapon@indeed.com>
2017-02-02 14:08:01 -0800

    complete shopping list excersice

--- shop.py
+++ shop.py
@@ -1,9 +1,19 @@
 items = []
+
+
 while (True):
-    print "If you are done with shopping list, type q to exit "
-    item = raw_input("add an item: ")
+    print ("If you are done with shopping list, type q to exit or type r to remove your list ")
+    print ('My list', sorted(items))
+    item = input("add an item: ")
     if(item == 'q'):
         break
+    elif (item in items): 
+        items.remove(item)
     else:
         items.append(item)
-print "Your items are ", items
+
+
+
+
+
+
